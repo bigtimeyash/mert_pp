748b2079db02c272108fdcb3b97af44356e4692d
Matthew Kongsiri <mkong@seas.upenn.edu>
2019-12-03 20:59:37 -0500

    got test server working, created Makefile for server

--- networking/server.cpp
+++ networking/server.cpp
@@ -1,23 +1,47 @@
-#include <httpserver.hpp>
+// #include <httpserver.hpp>
+// #include "../libhttpserver-master/src/httpserver.hpp"
 
-using namespace httpserver;
+// using namespace httpserver;
 
-class hello_world_resource : public http_resource
-{
-public:
-    const std::shared_ptr<http_response> render(const http_request &)
-    {
-        return std::shared_ptr<http_response>(new string_response("Hello, World!"));
-    }
-};
+// class hello_world_resource : public http_resource
+// {
+// public:
+//     const std::shared_ptr<http_response> render(const http_request &)
+//     {
+//         return std::shared_ptr<http_response>(new string_response("Hello, World!"));
+//     }
+// };
 
-int main(int argc, char **argv)
-{
-    webserver ws = create_webserver(8080);
+// int main(int argc, char **argv)
+// {
+//     webserver ws = create_webserver(8080);
 
-    hello_world_resource hwr;
-    ws.register_resource("/hello", &hwr);
-    ws.start(true);
+//     hello_world_resource hwr;
+//     ws.register_resource("/hello", &hwr);
+//     ws.start(true);
 
-    return 0;
+//     return 0;
+// }
+
+#include "../cpp-httplib-master/httplib.h"
+
+int main(void) {
+    using namespace httplib;
+
+    Server svr;
+
+    svr.Get("/hi", [](const Request& req, Response& res) {
+        res.set_content("Hello World!", "text/plain");
+    });
+
+    svr.Get(R"(/numbers/(\d+))", [&](const Request& req, Response& res) {
+        auto numbers = req.matches[1];
+        res.set_content(numbers, "text/plain");
+    });
+
+    svr.Get("/stop", [&](const Request& req, Response& res) {
+        svr.stop();
+    });
+
+    svr.listen("localhost", 1234);
 }
